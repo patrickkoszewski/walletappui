a1d515893a397188d06c8decbf2b70ee826a9abe
Patrick <ledrap1992@gmail.com>
2022-12-08 01:41:54 +0100

    Add Project Body

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//mając home_page i main dart w takiej formie możemy myślec nad pomysłem
+//nad ui lub się inspirować z neta i rozpisać po // jaki jest plan na wykonanie aplikacji
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+        //
+
+        //
+
+        //
+
+        //
+
+        );
+  }
+}
